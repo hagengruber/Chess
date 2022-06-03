81c148dc2b0ae32e2ce9be2cb8bf8e055c10832c
Sandro Schamberger <sandro.schamberger@stud.th-deg.de>
2022-06-03 14:41:56 +0200

    Added notes to TODO-section

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """
     @Author: Schamberger Sandro: 22102471
     @Author: Hagengruber Florian:
+    @Author: Joiko Christian:
 """
 from model import Model
 
@@ -17,6 +18,8 @@ TODO:
     -Add Tips, Rules, Options (however you want to call it) to allow user to change whether or not he wants to use
      symbols or not, tell him the rules/possibilities of this chess version (Castling etc. is possible) and to give tips
      (H is horse, Change Font-Size because symbols are very small) [Format of Input!]
-    -Add Algorithm and KI
     -Add Save Option
+    -Fix Bug that other pieces that are not Horses can jump over other pieces (Only happened once by KI with Pawn)
+    -Add comments to algorithm.py and evaluatePieces.py
+    -Test the game to find more bugs!
 """
